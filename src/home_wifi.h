d4e096bfa06f7d6d5082ad5ea96b41d373ab3397
Jack <git@jackhiggins.ie>
2020-04-27 15:02:53 +0200

    add header file

---
+++ src/home_wifi.h
@@ -0,0 +1,10 @@
+// Include this in other Arduino projects to configure common WiFi settings
+// https://github.com/skhg/home_wifi
+
+#ifndef HOME_WIFI_INCLUDED
+#define HOME_WIFI_INCLUDED
+
+const char* WIFI_SSID = "SSID Name"; //your WiFi Name
+const char* WIFI_PASSWORD = "Password";  //Your Wifi Password
+
+#endif
